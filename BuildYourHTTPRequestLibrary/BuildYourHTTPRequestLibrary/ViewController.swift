56a3dac563a9ef2b9eb6a4a92bc5da387b853ba5
张青明 <laznrbfe@zhangqingmingdeMacBook-Pro.local>
2015-06-25 17:44:31 +0800

    完成(一)。

--- BuildYourHTTPRequestLibrary/BuildYourHTTPRequestLibrary/ViewController.swift
+++ BuildYourHTTPRequestLibrary/BuildYourHTTPRequestLibrary/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mainButtonBeTapped(sender: AnyObject) {
+        let session = NSURLSession.sharedSession()
+        let request = NSURLRequest(URL: NSURL(string: "http://baidu.com")!)
+        let task    = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            
+            println("Just wait for 5 seconds!")
+            sleep(5)
+            
+            let string = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println(string)
+        })
+        task.resume()
+        
+    }
 
 }
 
